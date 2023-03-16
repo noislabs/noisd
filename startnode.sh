8f86af4f658d10920229ec6bd419ffa4eb37559c
jhernandezb <contact@jhernandez.me>
2023-03-16 09:16:29 -0600

    remove sed

--- startnode.sh
+++ startnode.sh
@@ -2,8 +2,6 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-# Use GNU version (Linux version) on Mac too. Otherwise the -i argument is incompatible.
-gnused="$(command -v gsed || echo sed)"
 
 # Create key under nickname "validator" first:
 # noisd keys add validator
@@ -14,7 +12,6 @@ rm -rf ~/.noisd/config/gentx
 # setup chain
 noisd init local-validator-001 --chain-id localnet-1 --overwrite
 noisd prepare-genesis localnet-1
-"$gnused" -i 's/stake/unois/' ~/.noisd/config/genesis.json
 
 noisd add-genesis-account "$VALIDATOR_ADDR" 500000000000000unois
 noisd gentx validator 1000000unois --chain-id localnet-1 --details "1"
