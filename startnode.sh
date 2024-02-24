1d48adcd8044634634744391b14893ecc1304204
Simon Warta <simon@warta.it>
2024-02-24 10:18:54 +0100

    Update add-genesis-account/gentx/collect-gentxs

--- startnode.sh
+++ startnode.sh
@@ -13,11 +13,11 @@ rm -rf ~/.noisd/config/gentx
 noisd init local-validator-001 --chain-id localnet-1 --overwrite
 noisd prepare-genesis localnet-1
 
-noisd add-genesis-account "$VALIDATOR_ADDR" 500000000000000unois
+noisd genesis add-genesis-account "$VALIDATOR_ADDR" 500000000000000unois
 # add initial community pool balance of 10000 NOIS
-noisd add-genesis-account nois103y4f6h80lc45nr8chuzr3fyzqywm9n0d8fxzu 10000000000unois
-noisd gentx validator 1000000unois --chain-id localnet-1 --details "1"
-noisd collect-gentxs
-noisd validate-genesis
+noisd genesis add-genesis-account nois103y4f6h80lc45nr8chuzr3fyzqywm9n0d8fxzu 10000000000unois
+noisd genesis gentx validator 1000000unois --chain-id localnet-1 --details "1"
+noisd genesis collect-gentxs
+noisd genesis validate-genesis
 noisd tendermint reset-state
 noisd tendermint unsafe-reset-all
